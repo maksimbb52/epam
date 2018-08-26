688150de011e51d42b149fa2199837357a09d34b
Maksim Baranov <maksim.bb52@gmail.com>
2018-08-27 00:25:23 +0300

    try to commit myConsole

---
+++ homeworks/custom/operationsWithFiles/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution.copy("d:\\3.txt", "d:\\1.txt");
+        Solution.createFile("d:\\1\\2\\3\\4\\4dffgh.txt");
+        Solution.viewAllCurrentDir("d:\\");
+        Solution.viewFilesCurrentDir("d:\\");
+        Solution.viewFilesDeep("d:\\1");
+    }
+}
---
+++ homeworks/custom/operationsWithFiles/src/Solution.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+public class Solution {
+    public static void rename(String before, String after) {
+        File file = new File(before);
+        try {
+            if (file.exists() && file.isFile()) {
+                file.renameTo(new File(after));
+                return;
+            }
+            System.out.println("File not exists");
+        } catch (SecurityException e) {
+            System.out.println("Incorrect one of paths.");
+        }
+    }
+
+    public static void copy(String existsFile, String newFile) {
+        File file = new File(existsFile);
+        try {
+            if (file.exists() && file.isFile()) {
+                Files.copy(Paths.get(existsFile), Paths.get(newFile), StandardCopyOption.COPY_ATTRIBUTES);
+                return;
+            }
+            System.out.println("File not exists.");
+        } catch (IOException | SecurityException e) {
+            System.out.println("Incorrect one of paths.");
+        }
+    }
+
+        public static void createFile(String path) {
+        try {
+            Path p = Paths.get(path);
+            Path temp = p.getRoot();
+            for (Path pa: p.getParent()) {
+                temp = Paths.get(temp.toString(), pa.toString());
+                System.out.println(temp.toString());
+                if (temp.toFile().exists() && !temp.toFile().isDirectory()) {
+                    temp.toFile().mkdir();
+                    continue;
+                }
+                if (!temp.toFile().exists()) {
+                    temp.toFile().mkdir();
+                    continue;
+                }
+            }
+            if (!p.toFile().exists()) {
+                p.toFile().createNewFile();
+                return;
+            }
+            System.out.println("File already exists");
+
+        } catch (IOException e) {
+            System.out.println("Incorrect path.");
+        }
+     }
+
+    public static void viewAllCurrentDir(String path) {
+        try (Stream<Path> paths = Files.list(Paths.get(path))) {
+            paths
+                    .forEach(System.out::println);
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Incorrect path.");
+        }
+    }
+
+    public static void viewFilesCurrentDir(String path) {
+        try (Stream<Path> paths = Files.list(Paths.get(path))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(System.out::println);
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Incorrect path.");
+        }
+    }
+
+    public static void viewFilesDeep(String path) {
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(System.out::println);
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Incorrect path.");
+        }
+    }
+
+}
